3fc857620e4c07e394c9e80796809fb9b3d7a27b
Chelsea Rice <officialchelsearice@gmail.com>
2025-03-03 17:17:02 -0500

    3-5

---
+++ C3.java
@@ -0,0 +1,14 @@
+public class C3{
+/**
+ * Write a Java program to divide two numbers and print them on the screen.
+ * Test Data :
+ * 50/3
+ * Expected Output :
+ * 16
+ */
+
+    public static void main (String [] args){
+        int quotient = 50/3;
+        System.out.println (quotient);
+    }
+}
---
+++ C5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class C5 {
+/**
+ * Write a Java program that takes two numbers as input and displays the product of two
+ * numbers.
+ * Test Data:
+ * Input first number: 25
+ * Input second number: 5
+ * Expected Output :
+ * 25 x 5 = 125
+ */
+
+ public static void main (String[] args){
+    Scanner scan = new Scanner (System.in);
+    System.out.println("Enter Int A : ");
+   
+
+    }
+}
