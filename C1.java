3f983d30e246d2e5be34122fb583cb41b37d8827
Chelsea Rice <officialchelsearice@gmail.com>
2025-03-03 15:26:57 -0500

    First 3 Problems
    Using website - https://www.w3resource.com/java-exercises/basic/index.php , do all the problems

---
+++ C1.java
@@ -0,0 +1,14 @@
+public class C1 {
+/**
+ * Write a Java program to print 'Hello' on screen and your name on a separate line.
+ * Expected Output
+ * Hello
+ * Chelsea Rice
+ * @param args
+ */
+public static void main (String[] args){
+    System.out.println("Hello");
+    System.out.println("Chelsea Rice");
+}
+
+}
---
+++ C2.java
@@ -0,0 +1,14 @@
+public class C2 {
+    /**
+     * Write a Java program to print the sum of two numbers.
+     * Test Data:
+     * 74 + 36
+     * Expected Output :
+     * 110
+     * @param args
+     */
+    public static void main (String [] args){
+        int sum = 74+36;
+        System.out.println("74 + 36 =" + " " +sum);
+    }
+}
